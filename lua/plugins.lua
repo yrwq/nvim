55dbaf0acf7bf41fa96286238198d7275835c1df
yrwq <yrwqid@gmail.com>
2026-02-04 23:32:21 +0100

    fix: todo hl

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,7 +97,30 @@ local init = {
     enabled = true,
     dependencies = "nvim-lua/plenary.nvim",
     config = function()
-      require("todo-comments").setup({})
+      require("todo-comments").setup({
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" }
+        },
+      })
     end
   },
 
@@ -107,7 +130,8 @@ local init = {
   --
 
   {
-    "https://github.com/yrwq/paper",
+    -- "https://github.com/yrwq/paper",
+    dir = "~/Developer/Repos/paper-nvim",
     config = function ()
       require("paper")
     end
